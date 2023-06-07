5b4a7c67507e9b09eb0903c76a8868d6314ac0b1
tanka1026 <tanka.bhattarai@infodev.com.np>
2023-06-07 11:17:42 +0545

    user create and update done

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import LoginView,UserCreateView,Dashboard,logout_user
+from .views import LoginView,UserRegisterView,Dashboard,logout_user,UserCreateView,UserUpdateView
 
 app_name="users"
 
@@ -10,8 +10,11 @@ urlpatterns = [
     path("", Dashboard.as_view(), name="dashboard"),
     path("login/", LoginView.as_view(), name="login_view"),
     path("logout/", logout_user, name="logout"),
+    path("register/", UserRegisterView.as_view(), name="register_view"),
+    path("user/create", UserCreateView.as_view(), name="user_create_view"),
 
-    path("register/", UserCreateView.as_view(), name="register_view"),
+    path("users/<int:pk>/update/", UserUpdateView.as_view(), name="user_update_view"),
+    # path("users/delete/", UserDelete.as_view(), name="user_delete"),
     
 ]
 
--- users/views.py
+++ users/views.py
@@ -4,17 +4,25 @@ from django.urls import reverse_lazy
 from django.contrib.auth.hashers import make_password
 from django.views import View
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views.generic import FormView,CreateView,TemplateView
-from .forms import LoginForm,UserCreateForm,CustomLoginForm
+from django.views.generic import FormView,CreateView,TemplateView,UpdateView
+from .forms import LoginForm,UserRegisterForm,CustomLoginForm,UserCreateForm,UserUpdateForm
 from users.permissions import IsLoggedInMixin
+from .models import AuthUser
 
 
 # Create your views here.
 
+class UserMixin(IsLoggedInMixin):
+    model = AuthUser
+    paginate_by = 10
+    queryset = AuthUser.objects.filter(is_deleted=False)
+    success_url = reverse_lazy("users:dashboard")
+
 class Dashboard(IsLoggedInMixin, TemplateView):
-    template_name = "index.html"
+    template_name = "user_list.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['users']=AuthUser.objects.all()
         return context
 
 def logout_user(request):
@@ -38,10 +46,9 @@ class LoginView(View):
             error_message = 'Invalid email or password'  
             return render(request, self.template_name, {'error_message': error_message})
         
-        
-
-class UserCreateView(CreateView):
-    form_class = UserCreateForm
+    
+class UserRegisterView(CreateView):
+    form_class = UserRegisterForm
     template_name = "registration/register.html"
     success_url = reverse_lazy("users:login_view")
 
@@ -59,3 +66,28 @@ class UserCreateView(CreateView):
     
     def form_invalid(self, form):
         return super().form_invalid(form)
+    
+
+class UserCreateView(UserMixin, CreateView):
+    form_class = UserCreateForm
+    template_name = "user_create.html"
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        password = form.cleaned_data['password']
+        email=form.cleaned_data.get("email")
+        form.instance.username=email
+        form.instance.is_staff=True
+        form.instance.is_superuser=True
+        user.password = make_password(password)
+        user.save()        
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        print("Erroor============",form.errors)
+        return super().form_invalid(form)
+    
+
+class UserUpdateView(UserMixin, UpdateView):
+    form_class = UserUpdateForm
+    template_name = "update.html"
