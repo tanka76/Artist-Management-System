8b481e48227e090d6b530896479b726d4e678bae
tanka1026 <tanka.bhattarai@infodev.com.np>
2023-06-07 09:52:30 +0545

    user logout and admin dashboard created

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import LoginView,UserCreateView,Dashboard
+from .views import LoginView,UserCreateView,Dashboard,logout_user
 
 app_name="users"
 
@@ -9,6 +9,8 @@ urlpatterns = [
 
     path("", Dashboard.as_view(), name="dashboard"),
     path("login/", LoginView.as_view(), name="login_view"),
+    path("logout/", logout_user, name="logout"),
+
     path("register/", UserCreateView.as_view(), name="register_view"),
     
 ]
